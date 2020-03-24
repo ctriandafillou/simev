cc591e00dbf2f7494266a58a256afa6f336c4dca
Catherine Triandafillou <ctriandafillou@uchicago.edu>
2020-03-24 11:28:46 -0500

    fixed script

--- analysis/generate_multiple_evols.sh
+++ analysis/generate_multiple_evols.sh
@@ -7,7 +7,7 @@ for i in 100 1000 10000
 do
 	for j in {1..10}
 	do
-	python example-evolver.py $j 1000 --output-frequency 10 -o output/more_ek/n$i/rep$j.out
+	python example-evolver.py $i 1000 --output-frequency 10 -o output/more_ek/n$i/rep$j.out
 	done
 done
 
