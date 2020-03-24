c0156735c0c0cf59114e307d17f03e961877b488
Catherine Triandafillou <ctriandafillou@uchicago.edu>
2020-03-24 11:20:44 -0500

    added script to run evolution sim multiple times

---
+++ analysis/generate_multiple_evols.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# A little loopy script to run the wf evolver multiple times
+
+
+for i in 100 1000 10000
+do
+	for j in {1..10}
+	do
+	python example-evolver.py $j 1000 --output-frequency 10 -o output/more_ek/n$i/rep$j.out
+	done
+done
+
